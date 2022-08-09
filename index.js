a10291c454c8dd8f04c6f204b60d28932103a4b6
zakiego <belummikir@gmail.com>
2022-08-10 01:32:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
+
+addEventListener('scheduled', event => {
+  event.waitUntil(handleScheduled())
+})
+
+import { Octokit } from '@octokit/core'
+
+async function handleScheduled() {
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+  })
+
+  const data = await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+    owner: 'mzakiyuddin',
+    repo: 'benchmark-cron',
+    event_type: 'cloudflare-worker',
+  })
+
+  console.log(await data.status)
+
+  return new Response(JSON.stringify({ ok: true }))
+}
